e709c2fdfa694c02df1971136592f122954edf6c
Shamaun-Nabi@ <dev.shamaun@gmail.com>
2023-11-01 17:08:12 +0600

    web chnage

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1 className='text-center text-3xl p-1 flex justify-center hover:bg-green-400 transition-colors duration-500'>Starter Template with vite tailwind router react icons toastify and react</h1>
-      <button className='p-4 btn btn-secondary' >Say hi!</button>
+      <button className='p-4 btn btn-secondary' >Say hi! new change</button>
       <ToastContainer position='top-right' />
     </>
   )
